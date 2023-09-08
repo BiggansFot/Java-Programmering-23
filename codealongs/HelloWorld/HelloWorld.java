6d780aa978a465f369565bc1021bb6b0db5e447d
BiggansFot <81302543+BiggansFot@users.noreply.github.com>
2023-09-08 09:02:15 +0200

    Trying to commit

--- codealongs/HelloWorld/HelloWorld.java
+++ codealongs/HelloWorld/HelloWorld.java
@@ -1,4 +1,5 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- codealongs/Print/finished/LearningPrint.java
+++ codealongs/Print/finished/LearningPrint.java
@@ -1,5 +1,17 @@
 package codealongs.Print.finished;
 
 public class LearningPrint {
-    
+    public static void main(String[] args) {
+        System.out.println('\u00E4');
+
+        int cars = 8;
+        String myName = "Pär";
+        System.out.println ("Hello, I am " + myName + " and I do not own " + cars + " cars.");
+
+        System.out.printf(myName, args)
+    }
+}
+
+class Parrot {
+    String name = "Gösta";
 }
